a92c4b5b8b8a1f9defd58c726c8365e7e91ccbd3
Muhammad Shakeeb Raza <shakeebraza90@gmail.com>
2025-03-14 20:03:41 -0500

    fix:"help logo chnage"

--- resources/views/theme/help/layout.blade.php
+++ resources/views/theme/help/layout.blade.php
@@ -3,6 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="{{ asset(getset('fav_icon')) }}" type="image/x-icon" rel="icon">
     <title>{{ getset('site_title') }} Help Center</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
     <!-- Font Awesome for icons -->
